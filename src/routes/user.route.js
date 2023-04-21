22e3ed8185783113c796f82a9a267f83a0d93a51
Jimmy Feriawan <feriawanjimmy@gmail.com>
2023-04-21 22:59:00 +0700

    refactor user route to limit update and delete

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,9 @@
 const userController = require("../controllers/user.controller");
 let router = require("express").Router();
 
-router.get("/", userController.findAllUser);
-router.get("/:id", userController.findOneUser);
-router.post("/", userController.createUser);
+// router.get("/", userController.findAllUser);
+// router.get("/:id", userController.findOneUser);
+// router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.put("/:id", userController.deleteUser);
 
